4b65cbc6e8e35defed2f005d722ce4be67c7600c
Florian Dreier <dreier@cqse.eu>
2020-01-14 17:45:17 +0100

    Update closure compiler to 20200101

--- es6-module-converter/build.gradle.kts
+++ es6-module-converter/build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
     implementation("com.google.guava:guava:28.1-jre")
     implementation("com.squareup.moshi:moshi:1.8.0")
 
-    implementation("com.google.javascript:closure-compiler:v20191111")
+    implementation("com.google.javascript:closure-compiler:v20200101")
 //    implementation("com.google.javascript:closure-compiler:1.0-SNAPSHOT")
 
     testImplementation("org.assertj:assertj-core:3.13.2")
